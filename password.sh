bfd3026a57b71741213f6991b5bf0568db5057ce
im2 <im@mars.com>
2021-05-19 21:26:46 +0800

    mvp 2, now can create a new user/password

--- password.sh
+++ password.sh
@@ -1,16 +1,40 @@
 #!/bin/bash
+#
+## @isaac
+# The purpose of this tool is to well the simplicity
+# and security of Keybase to manage your or your team's passwords easily
+##
+### Usage: passbase [options]
+##
+## Options:
+##   -h, --help    Display this message.
 
-value=`cat data/im@quantalucia.com_binance`
-echo "$value"
+# 1. check sqlite3 exist or install it
+if ! type sqlite3 > /dev/null;  then
+	sudo apt install sqlite3 -y
+else
+	echo "...db works"
+fi
 
+# confirm it's for private use or team
+kbuser=`keybase whoami`
+
+# check if the db file exists already
 dbfile='pass.db'
-#head $file -n 1 | sed "s/,/\n/g" | awk '{printf("%d %s\n", NR-1, $0)}'
 
-cmd1="sqlite3 database.db"
-cmd2="select * from databases"
+read -p 'Username: ' uservar
+read -p 'Password: ' passwordvar
+read -p 'Service(e.g. Google) ' servicevar
 
-`sqlite3 $dbfile "UPDATE n SET p = '$value' WHERE u= 'im@quantalucia.com' "`
+encrypted=`keybase encrypt $kbuser -m $passwordvar`
+#echo "$encrypted"
 
-result=`sqlite3 $dbfile "SELECT p FROM n WHERE u= 'im@quantalucia.com' LIMIT 1" `
-final=`keybase decrypt -m "$result"`
-echo $final
+dbcmd="sqlite3 database.db"
+sqlcmd="select * from databases"
+
+#`sqlite3 $dbfile "UPDATE n SET p = '$encrypted' WHERE u= 'im@quantalucia.com' "`
+`sqlite3 $dbfile "INSERT INTO n (u,p,s) values ('$uservar', '$encrypted', '$servicevar')"`
+
+result=`sqlite3 $dbfile "SELECT p FROM n WHERE u= '$uservar' LIMIT 1" `
+decrypted=`keybase decrypt -m "$result"`
+echo "The password:%s has been save",$decrypted
